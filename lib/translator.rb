7f953b50be0a83f8f6e5ea540901de5fb8a46e8e
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 18:41:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,19 @@
 
 def load_library
   # code goes here
+  emoticons = YAML.load_file(file_path)
+  
+  new_hash = {
+    'get_meaning' => {},
+    'get_emoticon' => {}
+  }
 
+  emoticons.each do |key,value|
+    new_hash['get_emoticon'][value[0]] = emoticons[key][1]
+    new_hash['get_meaning'][value[1]] = key
+  end
+
+  new_hash
 end
 
 def get_japanese_emoticon
