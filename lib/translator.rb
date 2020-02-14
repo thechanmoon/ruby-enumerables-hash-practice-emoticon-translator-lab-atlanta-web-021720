6e27ab83afa3c01a1351bda87678a85f9d64899c
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 18:45:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,3 +27,4 @@ def get_english_meaning(file_path, emoticon)
   # code goes here
   result = load_library(file_path)['get_meaning'][emoticon]
   result ? result : "Sorry, that emoticon was not found"
+end  
